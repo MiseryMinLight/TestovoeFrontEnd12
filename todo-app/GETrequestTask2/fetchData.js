2cf7a73900593dc1e28beb4a7d8ff82b08ad6962
Misery Light <miseryminlight@gmail.com>
2024-10-15 03:25:45 +0300

    TASK_2

---
+++ todo-app/GETrequestTask2/fetchData.js
@@ -0,0 +1,16 @@
+// fetchData.js
+
+const fetchPets = async () => {
+    try {
+        const response = await fetch('https://petstore3.swagger.io/api/v3/pet/findByStatus?status=available');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+fetchPets();
